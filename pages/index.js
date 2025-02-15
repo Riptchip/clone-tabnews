fefa7f82eb4b72b02726ebc0f86979758857de0c
Nathan Lima <nathanlima523@hotmail.com>
2025-02-14 23:44:47 -0300

    Atualiza página em construção para resumir o projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>Amanhã às 9hrs da manhã treino de vôlei</h1>;
+  return (
+    <div>
+      <h1>NEARRUON</h1>
+      <p>
+        Esse projeto está sendo realizado com o objetivo de trazer a internet
+        "de volta" ao seu objetivo: unir as pessoas com o objetivo de fazer do
+        mundo um lugar melhor.
+        <br></br>
+        Com um método contra intuitivo, a discussão, pois assim como a internet
+        ela foi desfigurada, muito até pela internet, mas só por meio de uma
+        saudável troca de ideias poderemos alcançar nossos objetivos.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
