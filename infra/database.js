a6614b86f346104f3b208ddaad2c99c8b3f2109e
Nathan Lima <nathanlima523@hotmail.com>
2025-02-23 16:18:14 -0300

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { preconnect } from "react-dom";
 
 async function query(queryObject) {
   const client = new Client({
@@ -7,7 +8,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   let connected = false;
@@ -29,3 +30,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
